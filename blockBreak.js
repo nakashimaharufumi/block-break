a261a90bc8177c8c701f495e69172cd2b2971e95
Nakashima Harufumi <nakashimaharufumi@gmail.com>
2022-05-30 15:43:45 +0900

    ブロック崩しゲーム

--- blockBreak.js
+++ blockBreak.js
@@ -9,8 +9,8 @@ let score = 0;
 
 let ballX = 350;
 let ballY = 650;
-let ballVx = 1+ parseInt(Math.random(6));
-let ballVy = (3 + parseInt(Math.random(7))) * -1;
+let ballVx = 3 + parseInt(Math.random(6));
+let ballVy = (5 + parseInt(Math.random(7))) * -1;
 let barX = 300;
 let barY = 660;
 
@@ -34,10 +34,10 @@ function draw() {
             textAlign(CENTER);
             textSize(25);
             text("click to start", 350, 400);
+            stage = 5;
             initField();
             if (mouseIsPressed) {
                 mouseIsPressed = false;
-                stage = 1;
                 for (let i=0; i<stage*5; i++) block.push(1);
                 cs = 1;
             }
@@ -58,6 +58,9 @@ function draw() {
             }
 
             if (stage==6) cs = 2;
+            textAlign(CENTER);
+            textSize(40);
+            text("STAGE: "+stage, 350, 40);
             break;
         case 2:
             textAlign(CENTER);
@@ -78,9 +81,6 @@ function draw() {
             }
             break;
     }
-    textAlign(CENTER);
-    textSize(50);
-    text("STAGE: "+stage, 350, 50);
 }
 
 function setField() {
@@ -97,8 +97,8 @@ function setField() {
 function initField() {
     ballX = 350;
     ballY = 650;
-    ballVx = 1+ parseInt(Math.random(6));
-    ballVy = (3 + parseInt(Math.random(7))) * -1;
+    ballVx = 3 + parseInt(Math.random(6));
+    ballVy = (5 + parseInt(Math.random(7))) * -1;
     barX = 300;
     barY = 660;
     rect(barX, barY, 100, 10);
@@ -106,9 +106,13 @@ function initField() {
 }
 
 function setBlock() {
+    block_x = [];
+    block_y = [];
     for (let cnt=0; cnt<stage*5; cnt++) {
         block_x.push(100*(1+(cnt%5)));
         block_y.push(100+parseInt(cnt/5)*40);
+        //textSize(10);
+        //text(block[cnt], block_x[cnt], block_y[cnt]);
         if (block[cnt]>0) rect(block_x[cnt], block_y[cnt], 90, 20);
     }
 }
@@ -127,9 +131,9 @@ function moveBall() {
     if (ballY<60) ballVy *= -1;
     if (ballY==740) cs = 3; //ゲームオーバー処理
     if (barX-10<ballX && barX+110>ballX && barY>ballY && barY-10<ballY) {
-        if (ballX<barX+25) ballVx = -1 * (2 + parseInt(Math.random(10)));
-        else if (ballX>barX+75) ballVx = 2 + parseInt(Math.random(10));
-        ballVy = (3 + parseInt(Math.random(7))) * -1;
+        if (ballX<barX+25) ballVx = -1 * (3 + parseInt(Math.random(10)));
+        else if (ballX>barX+75) ballVx = 3 + parseInt(Math.random(10));
+        ballVy = (5 + parseInt(Math.random(7))) * -1;
     }
     for (let cnt=0; cnt<stage*5; cnt++) {
         if (block[cnt]>0 && ((ballX>block_x[cnt]-10 && block_x[cnt]+100>ballX && block_y[cnt]+20<ballY && block_y[cnt]+30>ballY) || (ballX>block_x[cnt]-10 && block_x[cnt]+100>ballX && block_y[cnt]-10<ballY && block_y[cnt]>ballY))) {
